8c0e020ae5a9811b3790877b2c648bf6428e2fd2
MDario123 <manuel.dario.oliver@gmail.com>
2025-10-27 13:47:09 +0100

    add nix environment

---
+++ flake.nix
@@ -0,0 +1,27 @@
+{
+  inputs = {
+    nixpkgs.url = "github:nixos/nixpkgs?ref=nixos-unstable";
+  };
+
+  outputs = { self, nixpkgs }:
+    let
+      system = "x86_64-linux";
+      pkgs = import nixpkgs {
+        inherit system;
+      };
+    in
+    {
+      devShells.${system}.default = pkgs.stdenv.mkDerivation {
+        name = "c-shell";
+
+        buildInputs = with pkgs; [
+          clang-tools
+          clang
+        ];
+
+        shellHook = ''
+          export SHELL=zsh
+        '';
+      };
+    };
+}
